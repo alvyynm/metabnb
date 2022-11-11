84bfe5eb9ff964318056f3a94627f3a452b3f607
alvyynm <alvyynm@gmail.com>
2022-11-11 18:08:09 +0300

    feat: add toggle functionality to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../assets/logos/logo.png";
 
 function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <div className=" w-[90%] mx-auto lg:mx-[100px] mt-[43px] mb-[125px]">
       <nav className="flex justify-between">
         <div>
           <img src={logo} alt="metabnb logo" className="w-4/5" />
         </div>
-        <div className="lg:hidden">
+        <button
+          className="lg:hidden"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -25,8 +32,12 @@ function Navbar() {
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
-        </div>
-        <div className="hidden lg:inline-block">
+        </button>
+        <div
+          className={
+            isNavExpanded ? " lg:inline-block" : "hidden lg:inline-block"
+          }
+        >
           <ul className="flex lg:gap-12 text-xl text-primary">
             <li className="[text-shadow:0_4px_2px_rgba(0_0_0/_0.25)]">
               <Link to="/">Home</Link>
@@ -42,7 +53,11 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <div className="hidden lg:inline-block">
+        <div
+          className={
+            isNavExpanded ? " lg:inline-block" : "hidden lg:inline-block"
+          }
+        >
           <button className="text-white  bg-purple btn border-purple bg-gradient-to-r from-purple to-purple">
             Connect wallet
           </button>
