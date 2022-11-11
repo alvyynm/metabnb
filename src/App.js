253a5c254438029e44bb24c99810005d638dbc23
alvyynm <alvyynm@gmail.com>
2022-11-11 15:27:07 +0300

    feat: use react-router-dom for navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Places from "./pages/Places";
 
 function App() {
   return (
-    <div className="App font-primary">
-      <header className="App-header">
-        <p className="text-3xl font-bold underline">Hello world</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App font-primary">
+              <Home />
+            </div>
+          }
+        />
+        <Route
+          path="/places"
+          element={
+            <div className="App font-primary">
+              <Places />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="Navbar">
       <header className="Navbar-header">
         <p className="text-3xl font-bold underline">Hello nav</p>
+        <ul>
+          <li>
+            <Link to="/places">Places</Link>
+          </li>
+          <li>
+            <Link to="/">Homepage</Link>
+          </li>
+        </ul>
       </header>
     </div>
   );
